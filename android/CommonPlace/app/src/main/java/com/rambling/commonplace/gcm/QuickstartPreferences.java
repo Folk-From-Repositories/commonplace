a8185908fcfcfb1193901350f35325bd878540f7
kamyong kim <kamyong.kim@lgepartner.com>
2015-07-06 17:36:21 +0900

    Use google cloud message
    - ref : http://blog.saltfactory.net/android/implement-push-service-via-gcm.html

---
+++ android/CommonPlace/app/src/main/java/com/rambling/commonplace/gcm/QuickstartPreferences.java
@@ -0,0 +1,12 @@
+package com.rambling.commonplace.gcm;
+
+/**
+ * Created by raphael.kim on 7/6/15.
+ */
+public class QuickstartPreferences {
+
+    public static final String REGISTRATION_READY = "registrationReady";
+    public static final String REGISTRATION_GENERATING = "registrationGenerating";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
