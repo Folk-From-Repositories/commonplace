541f51bdc472b1620bf165b21bdc2f09df6db746
Rapahel Kim <sudokky.dev@gmail.com>
2015-07-16 09:21:35 +0900

    Recreate connection when disconnecting from database

--- server-nodejs/app/server/modules/account-manager.js
+++ server-nodejs/app/server/modules/account-manager.js
@@ -5,7 +5,32 @@ var mysql 		= require('mysql');
 var dbInfo 		= require(__dirname + '/../conf/database.json');
 
 /* establish the database connection */
-var connection = mysql.createConnection(dbInfo);
+var connection;
+
+function makeConnection() {
+	connection = mysql.createConnection(dbInfo);
+
+	connection.connect(function(err) {
+		if (err) {
+			console.error('Error when connecting to db:', err);
+			setTimeout(makeConnection, 3000);
+		} else {
+			console.log('Establish db connection.')
+		}
+	});
+
+	connection.on('error', function(err) {
+		console.error('Database error', err);
+
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			makeConnection();
+		} else {
+			throw err;
+		}
+	});
+}
+
+makeConnection();
 
 /* login validation methods */
 
