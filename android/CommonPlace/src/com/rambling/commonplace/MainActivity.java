959f60495d97a32a7a741bad1442102354057891
kmoo26c <kmoo26c@gmail.com>
2015-08-17 16:49:34 +0900

    Change MainActivity for opening Group View

--- android/CommonPlace/src/com/rambling/commonplace/MainActivity.java
+++ android/CommonPlace/src/com/rambling/commonplace/MainActivity.java
@@ -21,70 +21,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-//		Toast toast = Toast.makeText(getApplicationContext(), "TEST Activity", Toast.LENGTH_LONG);
-//		toast.setGravity(Gravity.CENTER, 0, 0);
-//		toast.show();
-		
-		//Intent intent = new Intent(this, GcmHandler.class);
 
-		findViewById(R.id.apple).setOnClickListener(mClickListener);
-		findViewById(R.id.orange).setOnClickListener(mClickListener);
-		
-		//startActivity(new Intent(this, InitManager.class));
-		//startActivity(new Intent(this, GetPhoneNumberList.class));
-		startActivity(new Intent(this, GroupMainView.class));
-		
-		  Log.d("KMC","1");
-		  Log.d("jonghun", "111112");
-		
-		
-		
-		//startActivityForResult(intent,0);
+		startActivityForResult(new Intent(this, InitManager.class), 0);
 	}
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    Log.d("KMC","resultCode: " + resultCode);
-
-//	    if(resultCode == RESULT_OK){
-//	    	findViewById(R.id.apple).setOnClickListener(mClickListener);
-//			findViewById(R.id.orange).setOnClickListener(mClickListener);
-//	    }
-//	    Vector<String> phoneArray = null;
-//		try{
-//			GetPhoneNumberList instance = new GetPhoneNumberList();
-//			phoneArray = ((GetPhoneNumberList)instance.getPhoneNumberListContext()).getPhoneNumbers();
-//		}catch(Exception e){
-//			StringWriter sw = new StringWriter();
-//		    e.printStackTrace(new PrintWriter(sw));
-//		    String exceptionAsStrting = sw.toString();
-//		    Log.e("KMC aa", exceptionAsStrting);
-//		    e.printStackTrace();
-//		}
-//		
-//		for(int i=0;i<phoneArray.size();i++){
-//			Log.d("KMC","phone Number: " + phoneArray.get(i));
-//		}
+	    Log.d("KMC","resultCode: " + RESULT_OK);
+	    
+	    if(resultCode == RESULT_OK){
+	    	startActivity(new Intent(this, GroupMainView.class));
+			finish();
+	    }
 	}
 	
-	Button.OnClickListener mClickListener = new View.OnClickListener() {
-		public void onClick(View v) {
-			TextView textFruit=(TextView)findViewById(R.id.fruit);
-			switch (v.getId()) {
-			case R.id.apple:
-				textFruit.setText("Apple");
-				Intent intent = new Intent(MainActivity.this, CreateMapView.class);
-				startActivity(intent);
-				finish();
-				break;
-			case R.id.orange:
-				textFruit.setText("Orange");
-				break;
-			}
-		}
-	};
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
